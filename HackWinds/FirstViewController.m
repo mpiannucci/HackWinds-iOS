f46a04d15a5ddeba2ade330554b525ba0000fa1d
Matthew Iannucci <mpiannucci@gmail.com>
2014-07-27 23:33:36 -0400

    Yayyyyyy i can do date things

--- HackWinds/FirstViewController.m
+++ HackWinds/FirstViewController.m
@@ -12,6 +12,7 @@
 @interface FirstViewController ()
 
 @property (weak, nonatomic) IBOutlet AsyncImageView *holderImageButton;
+@property (weak, nonatomic) IBOutlet UILabel *dayHeader;
 
 @end
 
@@ -23,6 +24,9 @@
     // Load the imageview
     NSURL *url = [NSURL URLWithString:@"http://www.warmwinds.com/wp-content/uploads/surf-cam-stills/image00001.jpg"];
     [_holderImageButton setImageURL:url];
+    NSDate *now = [[NSDate alloc] init];
+    NSString *day = [self getDayHeader:now];
+    [_dayHeader setText:day];
     
 }
 
@@ -30,6 +34,13 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSString *)getDayHeader:(NSDate *)date
+{
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"EEEE"];
+    return [dateFormatter stringFromDate:date];
 }
 
 @end
