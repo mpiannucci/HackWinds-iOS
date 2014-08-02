b3bb97287d84f64ae0ed08ee0fd7d29285c1fa18
Matthew Iannucci <mpiannucci@gmail.com>
2014-08-02 17:05:09 -0400

    Added msw background handler

--- HackWinds/FirstViewController.m
+++ HackWinds/FirstViewController.m
@@ -6,6 +6,10 @@
 //  Copyright (c) 2014 Rhodysurf Development. All rights reserved.
 //
 
+#define mswBgQueue dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+#define mswURL [NSURL URLWithString:@"http://magicseaweed.com/api/nFSL2f845QOAf1Tuv7Pf5Pd9PXa5sVTS/forecast/?spot_id=1103&fields=localTimestamp,swell.*,wind.*"]
+#define wwStillURL [NSURL URLWithString:@"http://www.warmwinds.com/wp-content/uploads/surf-cam-stills/image00001.jpg"]
+
 #import "FirstViewController.h"
 #import "AsyncImageView.h"
 
@@ -21,13 +25,22 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
     // Load the imageview
-    NSURL *url = [NSURL URLWithString:@"http://www.warmwinds.com/wp-content/uploads/surf-cam-stills/image00001.jpg"];
-    [_holderImageButton setImageURL:url];
+    [_holderImageButton setImageURL:wwStillURL];
+    
+    // Load the date
     NSDate *now = [[NSDate alloc] init];
     NSString *day = [self getDayHeader:now];
     [_dayHeader setText:day];
     
+    // Load the MSW Data
+    dispatch_async(mswBgQueue, ^{
+        NSData* data = [NSData dataWithContentsOfURL:
+                        mswURL];
+        [self performSelectorOnMainThread:@selector(fetchedMSWData:)
+                               withObject:data waitUntilDone:YES];
+    });
 }
 
 - (void)didReceiveMemoryWarning
@@ -41,6 +54,15 @@
     NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
     [dateFormatter setDateFormat:@"EEEE"];
     return [dateFormatter stringFromDate:date];
+}
+
+- (void)fetchedMSWData:(NSData *)responseData {
+    //parse out the MSW json data
+    NSError* error;
+    NSDictionary* json = [NSJSONSerialization
+                          JSONObjectWithData:responseData
+                          options:kNilOptions
+                          error:&error];
 }
 
 @end
