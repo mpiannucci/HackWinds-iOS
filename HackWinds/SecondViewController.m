ba683ed7511de9e53fb6d1163470005729476032
Matthew Iannucci <mpiannucci@gmail.com>
2014-09-14 11:08:55 -0400

    Added base layout for forecast view

--- HackWinds/SecondViewController.m
+++ HackWinds/SecondViewController.m
@@ -5,10 +5,14 @@
 //  Created by Matthew Iannucci on 7/18/14.
 //  Copyright (c) 2014 Rhodysurf Development. All rights reserved.
 //
+#define mswURL [NSURL URLWithString:@"http://magicseaweed.com/api/nFSL2f845QOAf1Tuv7Pf5Pd9PXa5sVTS/forecast/?spot_id=1103&fields=localTimestamp,swell.*,wind.*"]
 
 #import "SecondViewController.h"
 
+
 @interface SecondViewController ()
+
+@property (weak, nonatomic) IBOutlet UITableView *forecastTable;
 
 @end
 
@@ -24,6 +28,26 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    // Return so there will always be 5 rows
+    return 5;
+}
+
+- (UITableViewCell *)tableView: (UITableView *)tableView cellForRowAtIndexPath: (NSIndexPath *)indexPath
+{
+    // Get the interface items
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"forecastItem"];
+    
+    NSLog(@"Loaded cell");
+    // Return the cell view
+    return cell;
 }
 
 @end
