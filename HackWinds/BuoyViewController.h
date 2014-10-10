64cc562d43f1cb0110e881341425c51c56241665
mpiannucci <mpiannucci@gmail.com>
2014-10-09 22:09:50 -0400

    Adde buoy view controllers

---
+++ HackWinds/BuoyViewController.h
@@ -0,0 +1,13 @@
+//
+//  BuoyViewController.h
+//  HackWinds
+//
+//  Created by Matthew Iannucci on 10/9/14.
+//  Copyright (c) 2014 Rhodysurf Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BuoyViewController : UIViewController
+
+@end
