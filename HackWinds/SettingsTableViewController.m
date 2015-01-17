bd06aceb5523daefb0910a69b79bf1afb54930e2
Matthew Iannucci <mpiannucci@gmail.com>
2015-01-17 01:56:56 -0500

    Add beginning ui for setting view

---
+++ HackWinds/SettingsTableViewController.m
@@ -0,0 +1,41 @@
+//
+//  SettingsTableViewController.m
+//  HackWinds
+//
+//  Created by Matthew Iannucci on 1/17/15.
+//  Copyright (c) 2015 Rhodysurf Development. All rights reserved.
+//
+
+#import "SettingsTableViewController.h"
+
+@interface SettingsTableViewController ()
+{
+    NSArray* locationPickerData;
+}
+@end
+
+@implementation SettingsTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
