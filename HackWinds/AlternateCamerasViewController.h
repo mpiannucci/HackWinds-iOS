48545672f6736f133daf3f06f0573be4a7cbf933
Matthew Iannucci <mpiannucci@gmail.com>
2015-05-04 22:55:18 -0400

    Added initial blank alternate cmaeras view
    Removed old un-needed assets
    Updated linkage to newest core plot

---
+++ HackWinds/AlternateCamerasViewController.h
@@ -0,0 +1,15 @@
+//
+//  AlternateCamerasViewController.h
+//  HackWinds
+//
+//  Created by Matthew Iannucci on 5/4/15.
+//  Copyright (c) 2015 Rhodysurf Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AlternateCamerasViewController : UITableViewController
+
+- (IBAction)closeViewClicked:(id)sender;
+
+@end
