560275785667b94bf14b684f9b899e22ce53c0c1
Matthew Iannucci <mpiannucci@gmail.com>
2015-07-21 07:46:17 -0400

    Add first build with hackwindsdata framework
    IMplement tidemodel from hackwindsdata framework

--- HackWinds/Views/TideViewController.m
+++ HackWinds/Views/TideViewController.m
@@ -14,22 +14,25 @@
 #import "Buoy.h"
 #import "Colors.h"
 #import "Reachability.h"
+#import <hackwindsdata/Hackwindsdata.h>
 
 @interface TideViewController ()
 
-@property (strong, nonatomic) TideModel *tideModel;
 @property (strong, nonatomic) BuoyModel *buoyModel;
 
 @end
 
 @implementation TideViewController
+{
+    GoHackwindsdataTideModel *tideModel;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     // Get the tide model and buoy model
-    _tideModel = [TideModel sharedModel];
+    tideModel = GoHackwindsdataNewTideModel();
     _buoyModel = [BuoyModel sharedModel];
     
     // Get the buoy data and reload the views
@@ -38,7 +41,7 @@
     
     if (networkStatus != NotReachable) {
         dispatch_async(TIDE_FETCH_BG_QUEUE, ^{
-            [_tideModel getTideData];
+            [tideModel FetchTideData];
             [_buoyModel getBuoyDataForLocation:BLOCK_ISLAND_LOCATION];
             [self performSelectorOnMainThread:@selector(reloadView) withObject:nil waitUntilDone:YES];
         });
@@ -56,34 +59,34 @@
 
 - (void)reloadView {
     // If there are no tide items return early
-    NSMutableArray* tideData = [_tideModel getTideData];
-    NSMutableArray* buoyData = [_buoyModel getBuoyDataForLocation:BLOCK_ISLAND_LOCATION];
-    
-    if ([tideData count] == 0) {
+    if ([tideModel TideCount] == 0) {
         return;
     }
     
+    // We need the buoy model for the water temp
+    NSMutableArray* buoyData = [_buoyModel getBuoyDataForLocation:BLOCK_ISLAND_LOCATION];
+    
     int tideCount = 0;
-    for (int i = 0; i < [tideData count]; i++) {
+    for (int i = 0; i < [tideModel TideCount]; i++) {
         // Then check what is is again, and set correct text box
-        Tide* thisTide = [tideData objectAtIndex:i];
-        if ([[thisTide EventType] isEqualToString:SUNRISE_TAG]) {
+        GoHackwindsdataTide *tideEvent = [tideModel GetTideEventForIndex:i];
+        if ([[tideEvent EventType] isEqualToString:SUNRISE_TAG]) {
             // Get the first row in the sunrise and sunset section and set the text of the label to the time
             UILabel* sunriseLabel = (UILabel*)[[self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:2]] viewWithTag:61];
-            NSString* sunrisetext = [NSString stringWithFormat:@"Sunrise: %@", thisTide.Time];
+            NSString* sunrisetext = [NSString stringWithFormat:@"Sunrise: %@", tideEvent.Time];
             [sunriseLabel setText:sunrisetext];
             
-        } else if ([[thisTide EventType] isEqualToString:SUNSET_TAG]) {
+        } else if ([[tideEvent EventType] isEqualToString:SUNSET_TAG]) {
             // Get the second row in the sunrise and sunset section and set the text of the label to the time
             UILabel* sunsetLabel = (UILabel*)[[self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:1 inSection:2]] viewWithTag:61];
-            NSString* sunsetText = [NSString stringWithFormat:@"Sunset: %@", thisTide.Time];
+            NSString* sunsetText = [NSString stringWithFormat:@"Sunset: %@", tideEvent.Time];
             [sunsetLabel setText:sunsetText];
             
-        } else if ([[thisTide EventType] isEqualToString:HIGH_TIDE_TAG] ||
-                   [[thisTide EventType] isEqualToString:LOW_TIDE_TAG] ) {
+        } else if ([[tideEvent EventType] isEqualToString:HIGH_TIDE_TAG] ||
+                   [[tideEvent EventType] isEqualToString:LOW_TIDE_TAG] ) {
             // Get the next cell and its label so we can update it
             UILabel* tideLabel = (UILabel*)[[self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:tideCount inSection:1]] viewWithTag:51];
-            NSString* tideText = [NSString stringWithFormat:@"%@: %@ at %@", thisTide.EventType, thisTide.Height, thisTide.Time];
+            NSString* tideText = [NSString stringWithFormat:@"%@: %@ at %@", tideEvent.EventType, tideEvent.Height, tideEvent.Time];
             [tideLabel setText:tideText];
             
             // If its the first object, set it to the current status
@@ -91,12 +94,12 @@
                 // Get the current tide label
                 UILabel* currentTideLabel = (UILabel*)[[self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]] viewWithTag:41];
                 
-                if ([thisTide.EventType isEqualToString:HIGH_TIDE_TAG]) {
+                if ([tideEvent.EventType isEqualToString:HIGH_TIDE_TAG]) {
                     // Show that the tide is incoming, using green because typically surf increases with incoming tides
                     [currentTideLabel setText:@"Incoming"];
                     [currentTideLabel setTextColor:GREEN_COLOR];
                     
-                } else if ([thisTide.EventType isEqualToString:LOW_TIDE_TAG]) {
+                } else if ([tideEvent.EventType isEqualToString:LOW_TIDE_TAG]) {
                     // Show that the tide is outgoing, use red because the surf typically decreases with an outgoing tide
                     [currentTideLabel setText:@"Outgoing"];
                     [currentTideLabel setTextColor:RED_COLOR];
@@ -106,7 +109,7 @@
             tideCount++;
         }
     }
-    // TODO: Get the water temperature
+
     UILabel* currentWaterTempLabel = (UILabel*)[[self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:3]] viewWithTag:71];
     NSString* waterTemp = [[buoyData objectAtIndex:0] WaterTemperature];
     NSString* waterTempStatus = [NSString stringWithFormat:@"Block Island: %@ %@F", waterTemp, @"\u00B0"];
