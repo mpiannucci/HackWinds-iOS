4571b42dc21e86fe27fb0e57449911c0754949ba
Matthew Iannucci <mpiannucci@gmail.com>
2015-08-05 23:13:51 -0400

    Add empty today widget

---
+++ HackWindsTodayWidget/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  HackWindsTodayWidget
+//
+//  Created by Matthew Iannucci on 8/5/15.
+//  Copyright (c) 2015 Rhodysurf Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
