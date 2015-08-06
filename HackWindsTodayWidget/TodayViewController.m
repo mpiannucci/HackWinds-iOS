608ab213f1e4e9758374026805a84bac1ad5b45e
Matthew Iannucci <mpiannucci@gmail.com>
2015-08-06 00:31:10 -0400

    Fix today widget interface a little
    Update build number and version
    Still a little bug that pops up loading forecasts sometimes

--- HackWindsTodayWidget/TodayViewController.m
+++ HackWindsTodayWidget/TodayViewController.m
@@ -41,11 +41,11 @@
 
 - (void)widgetPerformUpdateWithCompletionHandler:(void (^)(NCUpdateResult))completionHandler {
     // Perform any setup necessary in order to update the view.
+    [self updateData];
     
     // If an error is encountered, use NCUpdateResultFailed
     // If there's no update required, use NCUpdateResultNoData
     // If there's an update, use NCUpdateResultNewData
-
     completionHandler(NCUpdateResultNewData);
 }
 
@@ -74,7 +74,8 @@
             } else {
                 tideCurrentStatus = @"Outgoing";
             }
-            nextTideEvent = [NSString stringWithFormat:@"%@ = %@", thisTide.Time, thisTide.EventType];
+            nextTideEvent = [NSString stringWithFormat:@"%@ - %@", thisTide.EventType, thisTide.Time];
+            break;
         }
         [self.tideCurrentStatusLabel setText:tideCurrentStatus];
         [self.nextTideEventLabel setText:nextTideEvent];
