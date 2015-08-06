f3bb5f538a25e43ee1ce55275e203757d5f0bc44
Matthew Iannucci <mpiannucci@gmail.com>
2015-08-05 20:55:54 -0400

    More robust buoy and tide tests

--- HackWindsDataKitTests/HackWindsDataKitTests.m
+++ HackWindsDataKitTests/HackWindsDataKitTests.m
@@ -46,12 +46,40 @@
 
 - (void) testBuoyModelFetch {
     XCTAssert([self.buoyModel fetchBuoyDataForLocation:BLOCK_ISLAND_LOCATION]);
-    XCTAssert([[self.buoyModel getBuoyDataForLocation:BLOCK_ISLAND_LOCATION] count] > 0);
+    XCTAssert([[self.buoyModel getBuoyDataForLocation:BLOCK_ISLAND_LOCATION] count] == 20);
+    
+    XCTAssert([self.buoyModel fetchBuoyDataForLocation:MONTAUK_LOCATION]);
+    XCTAssert([[self.buoyModel getBuoyDataForLocation:MONTAUK_LOCATION] count] == 20);
 }
 
 - (void) testTideModelFetch {
     XCTAssert([self.tideModel fetchTideData]);
+    
+    // Check for the correct amount of data points
     XCTAssert(self.tideModel.tides.count == 6);
+    
+    // Check for each data type there should be
+    int sunriseCount = 0;
+    int sunsetCount = 0;
+    int highTideCount = 0;
+    int lowTideCount = 0;
+    
+    for (Tide *tide in self.tideModel.tides) {
+        if ([tide isSunrise]) {
+            sunriseCount++;
+        } else if ([tide isSunset]) {
+            sunsetCount++;
+        } else if ([tide isHighTide]) {
+            highTideCount++;
+        } else if ([tide isLowTide]) {
+            lowTideCount++;
+        }
+    }
+    
+    XCTAssert(sunriseCount == 1);
+    XCTAssert(sunsetCount == 1);
+    XCTAssert(highTideCount == 2);
+    XCTAssert(lowTideCount == 2);
 }
 
 @end
