5814f1ffb32df46e24200fdad432c6639d909243
Matthew Iannucci <mpiannucci@gmail.com>
2015-09-10 23:18:16 -0400

    Add WaveWatch view controller to forecast view

---
+++ HackWinds/Views/WaveWatchChartViewController.h
@@ -0,0 +1,15 @@
+//
+//  WaveWatchChartViewController.h
+//  HackWinds
+//
+//  Created by Matthew Iannucci on 9/10/15.
+//  Copyright (c) 2015 Rhodysurf Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WaveWatchChartViewController : UIViewController
+
+- (IBAction)closeButtonClicked:(id)sender;
+
+@end
---
+++ HackWinds/Views/WaveWatchChartViewController.m
@@ -0,0 +1,17 @@
+//
+//  WaveWatchChartViewController.m
+//  HackWinds
+//
+//  Created by Matthew Iannucci on 9/10/15.
+//  Copyright (c) 2015 Rhodysurf Development. All rights reserved.
+//
+
+#import "WaveWatchChartViewController.h"
+
+@implementation WaveWatchChartViewController
+
+- (IBAction)closeButtonClicked:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+@end
