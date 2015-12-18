25bd9d9dc97a3a1320d4aa243f43a2f09e3eb14c
Matthew Iannucci <mpiannucci@gmail.com>
2015-12-18 16:00:36 -0500

    Add initial watchOS and OSX targets

---
+++ HackWindsDataKitOSX/HackWindsDataKitOSX.h
@@ -0,0 +1,19 @@
+//
+//  HackWindsDataKitOSX.h
+//  HackWindsDataKitOSX
+//
+//  Created by Matthew Iannucci on 12/18/15.
+//  Copyright © 2015 Rhodysurf Development. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for HackWindsDataKitOSX.
+FOUNDATION_EXPORT double HackWindsDataKitOSXVersionNumber;
+
+//! Project version string for HackWindsDataKitOSX.
+FOUNDATION_EXPORT const unsigned char HackWindsDataKitOSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HackWindsDataKitOSX/PublicHeader.h>
+
+
---
+++ HackWindsDataKitWatchOS/HackWindsDataKitWatchOS.h
@@ -0,0 +1,19 @@
+//
+//  HackWindsDataKitWatchOS.h
+//  HackWindsDataKitWatchOS
+//
+//  Created by Matthew Iannucci on 12/18/15.
+//  Copyright © 2015 Rhodysurf Development. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for HackWindsDataKitWatchOS.
+FOUNDATION_EXPORT double HackWindsDataKitWatchOSVersionNumber;
+
+//! Project version string for HackWindsDataKitWatchOS.
+FOUNDATION_EXPORT const unsigned char HackWindsDataKitWatchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HackWindsDataKitWatchOS/PublicHeader.h>
+
+
