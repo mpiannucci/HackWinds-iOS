65fc3a1dbda10270ee2d117a0bd686f7e813bd3a
Matthew Iannucci <mpiannucci@gmail.com>
2015-12-22 22:22:17 -0500

    Got bridging header to swift all set up

---
+++ HackWindsWatchApp Extension/HackWindsWatch-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  HackWindsWatch-Bridging-Header.h
+//  HackWinds
+//
+//  Created by Matthew Iannucci on 12/22/15.
+//  Copyright © 2015 Rhodysurf Development. All rights reserved.
+//
+
+#ifndef HackWindsWatch_Bridging_Header_h
+#define HackWindsWatch_Bridging_Header_h
+
+#import "HackWindsDataKitWatchOS.h"
+
+
+#endif /* HackWindsWatch_Bridging_Header_h */
