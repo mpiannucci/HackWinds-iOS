20b3d943c742e2f5b9b7947937b74c785a7df1ae
Matthew Iannucci <mpiannucci@gmail.com>
2016-01-02 13:22:21 -0500

    New OSX targets

---
+++ HackWindsOSX/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HackWindsOSX
+//
+//  Created by Matthew Iannucci on 1/2/16.
+//  Copyright © 2016 Rhodysurf Development. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ HackWindsTodayOSX/TodayViewController.swift
@@ -0,0 +1,25 @@
+//
+//  TodayViewController.swift
+//  HackWindsTodayOSX
+//
+//  Created by Matthew Iannucci on 1/2/16.
+//  Copyright © 2016 Rhodysurf Development. All rights reserved.
+//
+
+import Cocoa
+import NotificationCenter
+
+class TodayViewController: NSViewController, NCWidgetProviding {
+
+    override var nibName: String? {
+        return "TodayViewController"
+    }
+
+    func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)!) {
+        // Update your data and prepare for a snapshot. Call completion handler when you are done
+        // with NoData if nothing has changed or NewData if there is new data since the last
+        // time we called you
+        completionHandler(.NoData)
+    }
+
+}
