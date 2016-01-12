58dd6c568d19bfe5d5f8eb15fe372b5593220e0c
Matthew Iannucci <mpiannucci@gmail.com>
2016-01-11 20:13:33 -0500

    Remove objective c today widget
    Add swift iOS today target
    Bump to build 83

---
+++ HackWindsToday/HackWinds-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  HackWinds-Bridging-Header.h
+//  HackWinds
+//
+//  Created by Matthew Iannucci on 1/11/16.
+//  Copyright © 2016 Rhodysurf Development. All rights reserved.
+//
+
+#ifndef HackWinds_Bridging_Header_h
+#define HackWinds_Bridging_Header_h
+
+#import "HackWindsDataKit.h"
+
+#endif /* HackWinds_Bridging_Header_h */
