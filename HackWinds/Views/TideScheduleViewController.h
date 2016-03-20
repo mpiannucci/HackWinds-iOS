2ebfd285004650343623459a08406a88bfa1d8a2
Matthew Iannucci <mpiannucci@gmail.com>
2016-03-20 18:57:27 -0400

    Add empty tide schedule view

---
+++ HackWinds/Views/TideScheduleViewController.h
@@ -0,0 +1,14 @@
+//
+//  TideScheduleViewController.h
+//  HackWinds
+//
+//  Created by Matthew Iannucci on 3/20/16.
+//  Copyright © 2016 Rhodysurf Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TideScheduleViewController : UITableViewController
+
+- (IBAction)closeView:(id)sender;
+@end
---
+++ HackWinds/Views/TideScheduleViewController.m
@@ -0,0 +1,17 @@
+//
+//  TideScheduleViewController.m
+//  HackWinds
+//
+//  Created by Matthew Iannucci on 3/20/16.
+//  Copyright © 2016 Rhodysurf Development. All rights reserved.
+//
+
+#import "TideScheduleViewController.h"
+
+@implementation TideScheduleViewController
+
+- (IBAction)closeView:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+@end
