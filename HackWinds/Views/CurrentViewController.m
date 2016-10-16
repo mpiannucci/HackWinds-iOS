03df8154d3825880027409966bb9dd09b1b95ebe
Matthew Iannucci <mpiannucci@gmail.com>
2016-10-16 10:29:28 -0400

    Working rotation for currnt view, only shows the camera images

--- HackWinds/Views/CurrentViewController.m
+++ HackWinds/Views/CurrentViewController.m
@@ -205,6 +205,12 @@ static const int CAMERA_IMAGE_COUNT = 11;
     }
 }
 
+- (void) clearCameraPages {
+    for (int index = 0; index < CAMERA_IMAGE_COUNT; index++) {
+        [self removePageForIndex:index];
+    }
+}
+
 - (void) loadCameraPageForIndex:(int)index {
     if ((index < 0) || (index >= CAMERA_IMAGE_COUNT)) {
         return;
@@ -346,6 +352,30 @@ static const int CAMERA_IMAGE_COUNT = 11;
         swellLabel.font = [UIFont systemFontOfSize:17.0];
     }
     return cell;
+}
+
+#pragma mark - Rotation
+
+- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id <UIViewControllerTransitionCoordinator>)coordinator
+{
+    [super viewWillTransitionToSize:size withTransitionCoordinator:coordinator];
+    
+    // Code here will execute before the rotation begins.
+    // Equivalent to placing it in the deprecated method -[willRotateToInterfaceOrientation:duration:]
+    
+    [coordinator animateAlongsideTransition:^(id<UIViewControllerTransitionCoordinatorContext> context) {
+        
+        // Place code here to perform animations during the rotation.
+        // You can pass nil or leave this block empty if not necessary.
+        
+    } completion:^(id<UIViewControllerTransitionCoordinatorContext> context) {
+        
+        // Code here will execute after the rotation has finished.
+        // Equivalent to placing it in the deprecated method -[didRotateFromInterfaceOrientation:]
+        [self clearCameraPages];
+        [self loadCameraPages];
+        
+    }];
 }
 
 @end
